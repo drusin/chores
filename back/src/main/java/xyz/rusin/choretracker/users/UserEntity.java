663ef2bb82537dc00f7f7e517b0e69a4605683ae
drusin <dawidrusin90@gmail.com>
2025-10-17 16:55:21 +0200

    fix problem in entity generation

--- back/src/main/java/xyz/rusin/choretracker/users/UserEntity.java
+++ back/src/main/java/xyz/rusin/choretracker/users/UserEntity.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
@@ -12,7 +13,7 @@ import lombok.Data;
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class UserEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  Long id;
     private String name;
     private String imageName;
